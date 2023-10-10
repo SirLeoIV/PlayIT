65dec4c1f18b9b25dc74326a7a0703474efbb269
Sir Leo IV <leo-ebeling@web.de>
2023-10-10 09:54:50 +0200

    add rules to the rules-dialog of the menu

--- src/main/java/com/group09/playit/gui/MenuSceneController.java
+++ src/main/java/com/group09/playit/gui/MenuSceneController.java
@@ -101,7 +101,17 @@ public class MenuSceneController {
     void openRules(ActionEvent event) {
         rulesDialog = new Dialog<>();
         rulesDialog.setTitle("Rules");
-        rulesDialog.setContentText("These are the rules of the game Hearts.");
+        rulesDialog.setContentText("Welcome to our Game of Hearts.\n\n" +
+                "The objective of the game is to be the player with the lowest score. Once a player’s score is higher than the losing score, the game is over, and the player with the lowest score wins.\n\n" +
+                "You play in rounds. All cards are dealt in the beginning of each round. The player who has the 2 of clubs begins. Each player must follow the suit of the first card played, if possible. " +
+                "If not, then the player may discard any card of any suit. The highest card of the suit led wins the trick, and the winner of that trick leads next. \n\n" +
+                "Each time you win a trick, the trick is collected and placed face down in front of you. You add points to your score if there are hearts (1 point each) or the queen of spades (13 points) present. \n\n" +
+                "You cannot lead a trick with hearts until a heart has been discarded (hearts is broken). The queen can be led any time. " +
+                "If you have a card of the leading suit, you must play it. You can set the losing score to your liking. \n\n" +
+                "For multiplayer: Each player must confirm they are playing before taking a turn. Turn the laptop facing away from the other players. Have fun!!");
+        rulesDialog.setResizable(true);
+        rulesDialog.setWidth(400);
+        rulesDialog.setHeight(600);
         rulesDialog.getDialogPane().getScene().getWindow().setOnCloseRequest(e -> {
             rulesDialog.close();
             System.out.println("Dialog was closed");
