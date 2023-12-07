0b6e0779bb572ea84c80c6af692d856fc77168ac
Lauria Rubega <112666834+Lauria12@users.noreply.github.com>
2023-12-07 17:39:10 +0100

    Files that have the MCTSAgent-Random experiments

--- src/main/java/com/group09/playit/experiments/Experiment.java
+++ src/main/java/com/group09/playit/experiments/Experiment.java
@@ -24,10 +24,10 @@ public class Experiment {
     // }
 
     public static void main(String[] args) {
-        String[] playerNames = new String[]{"MCTSAgent", "XXXXAgent 1", "XXXXAgent 2", "XXXXAgent 3"}; // Change player names here
-        String fileName = "MCTSAgent-XXXXAgent"; // Change file name here
+        String[] playerNames = new String[]{"MCTSAgent", "RandomAgent 1", "RandomAgent 2", "RandomAgent 3"}; // Change player names here
+        String fileName = "MCTSAgent-RandomAgent"; // Change file name here
         Agent agent1 = new MCTSAgent(0,null);
-        Agent agent2 = new SimpleAgent(0,null); // Change agent here
+        Agent agent2 = new RandomAgent(0,null); // Change agent here
 
         ArrayList<ArrayList<Integer>> scores = new ArrayList<>();
         createFile(fileName,
